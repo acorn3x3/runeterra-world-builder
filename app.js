e5d966138e3c62e63bc4e85feb6b65ea929b9080
Andrew Cornejo <atcornejo3@gmail.com>
2022-10-10 18:15:06 -0700

    added city picture tied to encounter.city element

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ const adjsList = document.getElementById('adjs-list');
 const encounterHeader = document.getElementById('encounter-header');
 const userChampion = document.getElementById('user-champion');
 const adjInput = document.getElementById('adj-input');
+const userCity = document.getElementById('user-city');
 
 /* State */
 let encounter = {
     name: 'Encounter',
-    champion: '',
-    city: '',
+    champion: 'aatrox',
+    city: 'demacia',
     adjs: [],
 };
 
@@ -33,7 +34,7 @@ championSelect.addEventListener('change', () => {
 });
 
 citySelect.addEventListener('change', () => {
-    encounter.champion = citySelect.value;
+    encounter.city = citySelect.value;
     displayEncounter();
 });
 
@@ -50,10 +51,16 @@ function displayEncounter() {
     userArticle.classList.value = encounter.city;
     encounterHeader.textContent = encounter.name;
     userChampion.src = 'assets/' + encounter.champion + '.png';
+    userCity.src = 'assets/' + encounter.city + '.jpg';
 }
 
 function displayAdjs() {
     adjsList.innerHTML = '';
+    for (let adj of encounter.adjs) {
+        const li = document.createElement('li');
+        li.textContent = adj;
+        adjsList.append(li);
+    }
 }
 
 displayEncounter();
